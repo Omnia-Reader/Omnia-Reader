38858a850894ae1feff7e336a57f3cb9f9c07e82
Maurizio Casciano <mauriziocasciano7@gmail.com>
2025-12-31 23:25:10 +0100

    npx nx generate @nx/angular:component viewer
    npx nx generate @nx/angular:component apps/omnia-reader/src/app/components/viewer --standalone --style=scss

---
+++ apps/omnia-reader/src/app/components/viewer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'omnia-viewer',
+  imports: [],
+  templateUrl: './viewer.html',
+  styleUrl: './viewer.scss',
+})
+export class Viewer {}
